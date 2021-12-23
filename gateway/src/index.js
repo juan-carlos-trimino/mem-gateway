5d7037fa2d514fd0838b09debebbdc130c540b12
Juan Carlos Trimino <juancarlos@trimino.com>
2021-12-23 13:26:02 -0600

    Add code to continue running after exception.

--- gateway/src/index.js
+++ gateway/src/index.js
@@ -21,6 +21,29 @@ const MAX_RETRIES = process.env.MAX_RETRIES && parseInt(process.env.MAX_RETRIES)
 let READINESS_PROBE = false;
 
 /***
+Resume Operation
+----------------
+The resume operation strategy intercepts unexpected errors and responds by allowing the process to
+continue.
+***/
+process.on("uncaughtException",
+err => {
+  console.error("Uncaught exception:");
+  console.error(err && err.stack || err);
+})
+
+/***
+Abort and Restart
+-----------------
+***/
+// process.on("uncaughtException",
+// err => {
+//   console.error("Uncaught exception:");
+//   console.error(err && err.stack || err);
+//   process.exit(1);
+// })
+
+/***
 Unlike most other programming languages or runtime environments, Node.js doesn't have a built-in
 special "main" function to designate the entry point of a program.
 
@@ -29,64 +52,51 @@ Accessing the main module
 When a file is run directly from Node.js, require.main is set to its module. That means that it is
 possible to determine whether a file has been run directly by testing require.main === module.
 ***/
-if (require.main === module)
-{
+if (require.main === module) {
   main()
-  .then(() =>
-  {
+  .then(() => {
     READINESS_PROBE = true;
     console.log(`Microservice "gateway" is listening on port "${PORT}"!`);
   })
-  .catch(err =>
-  {
+  .catch(err => {
     console.error('Microservice "gateway" failed to start.');
     console.error(err && err.stack || err);
   });
 }
 
-function main()
-{
+function main() {
   //Throw an exception if any required environment variables are missing.
-  if (process.env.SVC_DNS_METADATA === undefined)
-  {
+  if (process.env.SVC_DNS_METADATA === undefined) {
     throw new Error('Please specify the service DNS in the environment variable SVC_DNS_METADATA.');
   }
-  else if (process.env.SVC_DNS_HISTORY === undefined)
-  {
+  else if (process.env.SVC_DNS_HISTORY === undefined) {
     throw new Error('Please specify the service DNS in the environment variable SVC_DNS_HISTORY.');
   }
-  else if (process.env.SVC_DNS_VIDEO_UPLOAD === undefined)
-  {
+  else if (process.env.SVC_DNS_VIDEO_UPLOAD === undefined) {
     throw new Error('Please specify the service DNS in the environment variable SVC_DNS_VIDEO_UPLOAD.');
   }
-  else if (process.env.SVC_DNS_VIDEO_STREAMING === undefined)
-  {
+  else if (process.env.SVC_DNS_VIDEO_STREAMING === undefined) {
     throw new Error('Please specify the service DNS in the environment variable SVC_DNS_VIDEO_STREAMING.');
   }
   //Display a message if any optional environment variables are missing.
-  else
-  {
-    if (process.env.PORT === undefined)
-    {
+  else {
+    if (process.env.PORT === undefined) {
       console.log('The environment variable PORT for the "HTTP server" is missing; using port 3000.');
     }
     //
-    if (process.env.MAX_RETRIES === undefined)
-    {
+    if (process.env.MAX_RETRIES === undefined) {
       console.log(`The environment variable MAX_RETRIES is missing; using MAX_RETRIES=${MAX_RETRIES}.`);
     }
   }
   //Notify when server has started.
-  return new Promise(resolve =>
-  {
+  return new Promise(resolve => {
     //Set directory that contains templates for views.
     app.set('views', path.join(__dirname, 'views'));
     //Use hbs as the view engine for Express.
     app.set('view engine', 'hbs');
     app.use(express.static("public"));
     app.listen(PORT,
-    () =>
-    {
+    () => {
       resolve();  //HTTP server is listening, resolve the promise.
     });
   });
@@ -94,10 +104,8 @@ function main()
 
 //Readiness probe.
 app.get('/readiness',
-(req, res) =>
-{
-  const options =
-  {
+(req, res) => {
+  const options = {
     host: SVC_DNS_METADATA,
     path: '/readiness',
     method: 'GET'
@@ -105,23 +113,19 @@ app.get('/readiness',
   console.log(`Connecting to "metadata" at ${SVC_DNS_METADATA}.`);
   //Check upstream dependencies.
   http.request(options,
-  (response) =>
-  {
-    if (response.statusCode === 200)
-    {
+  (response) => {
+    if (response.statusCode === 200) {
       READINESS_PROBE = true;
       res.sendStatus(200);
     }
-    else
-    {
+    else {
       READINESS_PROBE = false;
       console.log(`Upstream dependency ${SVC_DNS_METADATA} not ready.`);
       res.sendStatus(500);
     }
   })
   .on('error',
-  err =>
-  {
+  err => {
     READINESS_PROBE = false;
     console.log(`Upstream dependency ${SVC_DNS_METADATA} not ready.`);
     res.sendStatus(500);
@@ -131,28 +135,23 @@ app.get('/readiness',
 
 //Main web page for listing videos.
 app.get('/',
-(req, res) =>
-{
+(req, res) => {
   console.log('List the videos.');
-  const options =
-  {
+  const options = {
     host: SVC_DNS_METADATA,
     path: '/videos',
     method: 'GET'
   };
   //Get the list of videos from the metadata microservice.
   http.request(options,
-  (response) =>
-  {
+  (response) => {
     let data = '';
     response.on('data',
-    chunk =>
-    {
+    chunk => {
       data += chunk;
     });
     response.on('end',
-    () =>
-    {
+    () => {
       /***
       An empty string is not a valid JSON string; JSON.parse(''); will not parse correctly.
       While an empty string is not valid JSON, two quotes is valid JSON; JSON.parse('""');
@@ -167,30 +166,25 @@ app.get('/',
         The null value 'null'
       ***/
       //Render the video list for display in the browser.
-      if (response.statusCode === 500)
-      {
+      if (response.statusCode === 500) {
         res.render('video-list', { videos: JSON.parse('[]') });
       }
-      else if (data === undefined || data === null || data === '')
-      {
+      else if (data === undefined || data === null || data === '') {
         res.render('video-list', { videos: JSON.parse('[]') });
       }
-      else
-      {
+      else {
         res.render('video-list', { videos: JSON.parse(data).videos });
       }
     });
     response.on('error',
-    err =>
-    {
+    err => {
       console.error('Failed to get the video list.');
       console.error(err || `Status code: ${response.statusCode}`);
       res.sendStatus(500);
     });
   })
   .on('error',
-  err =>
-  {
+  err => {
     console.error('Failed to get the video list.');
     console.error(err || `Status code: ${response.statusCode}`);
     res.sendStatus(500);
@@ -200,43 +194,35 @@ app.get('/',
 
 //Web page for playing a particular video.
 app.get('/video',
-(req, res) =>
-{
+(req, res) => {
   const videoId = req.query.id;
   console.log(`Request video ${videoId} to play.`);
   //Get the selected video from the metadata microservice.
-  http.request(
-  {
+  http.request({
     host: SVC_DNS_METADATA,
     path: `/video?id=${videoId}`,
     method: 'GET'
   },
-  (response) =>
-  {
+  (response) => {
     let data = '';
     response.on('data',
-    chunk =>
-    {
+    chunk => {
       data += chunk;
     });
     response.on('end',
-    () =>
-    {
-      try
-      {
+    () => {
+      try {
         const metadata = JSON.parse(data).video;
         const video = { metadata, url: `/api/video?id=${videoId}` };
         //Render the video for display in the browser.
         res.render('play-video', { video });
       }
-      catch
-      {
+      catch {
         console.error(`Failed to get details for video ${videoId}.`);
       }
     });
     response.on("error",
-    err =>
-    {
+    err => {
       console.error(`Failed to get details for video ${videoId}.`);
       console.error(err || `Status code: ${response.statusCode}`);
       res.sendStatus(500);
@@ -247,51 +233,41 @@ app.get('/video',
 
 //Web page for uploading a new video.
 app.get('/upload',
-(req, res) =>
-{
+(req, res) => {
   res.render('upload-video', {});
 });
 
 //Web page for showing the users viewing history.
 app.get('/history',
-(req, res) =>
-{
+(req, res) => {
   console.log('Request viewing history.');
   //Get the viewing history from the history microservice.
-  http.request(
-  {
+  http.request({
     host: SVC_DNS_HISTORY,
     path: '/videos',
     method: 'GET'
   },
-  (response) =>
-  {
+  (response) => {
     let data = '';
     response.on('data',
-    chunk =>
-    {
+    chunk => {
       data += chunk;
     });
     response.on('end',
-    () =>
-    {
+    () => {
       //Render the history for display in the browser.
-      if (response.statusCode === 500)
-      {
+      if (response.statusCode === 500) {
         res.render('history', { videos: JSON.parse('[]') });
       }
-      else if (data === undefined || data === null || data === '')
-      {
+      else if (data === undefined || data === null || data === '') {
         res.render('history', { videos: JSON.parse('[]') });
       }
-      else
-      {
+      else {
         res.render('history', { videos: JSON.parse(data).videos });
       }
     });
     response.on('error',
-    err =>
-    {
+    err => {
       console.error('Failed to get history.');
       console.error(err || `Status code: ${response.statusCode}`);
       res.sendStatus(500);
@@ -302,18 +278,15 @@ app.get('/history',
 
 //HTTP GET API to stream video to the user's browser.
 app.get('/api/video',
-(req, res) =>
-{
+(req, res) => {
   console.log(`Stream video ${req.query.id} to the browser.`);
   //Forward the request to the video streaming microservice.
-  const forwardReq = http.request(
-  {
+  const forwardReq = http.request({
     host: SVC_DNS_VIDEO_STREAMING,
     path: `/video?id=${req.query.id}`,
     method: 'GET'
   },
-  forwardRes =>
-  {
+  forwardRes => {
     res.writeHeader(forwardRes.statusCode, forwardRes.headers);
     forwardRes.pipe(res);
   });
@@ -322,18 +295,15 @@ app.get('/api/video',
 
 //HTTP POST API to upload video from the user's browser.
 app.post('/api/upload',
-(req, res) =>
-{
+(req, res) => {
   //Forward the request to the video streaming microservice.
-  const forwardReq = http.request(
-  {
+  const forwardReq = http.request({
     host: SVC_DNS_VIDEO_UPLOAD,
     path: '/upload',
     method: 'POST',
     headers: req.headers
   },
-  forwardRes =>
-  {
+  forwardRes => {
     res.writeHeader(forwardRes.statusCode, forwardRes.headers);
     forwardRes.pipe(res);
   });
@@ -350,8 +320,7 @@ that none of them responded. All you need to do is add a middleware function at 
 the stack (below all other functions) to handle a 404 response.
 ***/
 app.use(
-(req, res, next) =>
-{
+(req, res, next) => {
   console.error(`Unable to find the requested resource (${req.url})!`);
   res.status(404).send(`<h1>Unable to find the requested resource (${req.url})!</h1>`);
 });
